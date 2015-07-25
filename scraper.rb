37f05651e5e13142047f61401cfeefb6efdf4070
Tony Bowden <tony@tmtm.com>
2015-07-25 09:08:27 +0100

    Add wikidata

--- scraper.rb
+++ scraper.rb
@@ -5,6 +5,7 @@ require 'scraperwiki'
 require 'nokogiri'
 require 'open-uri'
 require 'colorize'
+require 'wikidata'
 
 require 'pry'
 require 'open-uri/cached'
@@ -18,6 +19,33 @@ end
 
 def noko_for(url)
   Nokogiri::HTML(open(url).read) 
+end
+
+def wikidata(title)
+  return {} if title.to_s.empty?
+
+  wd = Wikidata::Item.find_by_title title
+
+  property = ->(elem, attr='title') { 
+    prop = wd.property(elem) or return
+    prop.send(attr)
+  }
+
+  fromtime = ->(time) { 
+    return unless time
+    DateTime.parse(time.time).to_date.to_s 
+  }
+
+  # party = P102
+  # freebase = P646
+  return { 
+    wikidata: wd.id,
+    family_name: property.('P734'),
+    given_name: property.('P735'),
+    image: property.('P18', 'url'),
+    gender: property.('P21'),
+    birth_date: fromtime.(property.('P569', 'value')),
+  }
 end
 
 def scrape_list(url)
@@ -37,10 +65,12 @@ def scrape_list(url)
         constituency: constituency,
         votes: tds[2].text.to_i,
       }
+      data[:wikipedia] = URI.join('https://en.wikipedia.org/', data[:wikipedia]).to_s unless data[:wikipedia].to_s.empty?
       # https://en.wikipedia.org/wiki/Mitiaro_by-election_2014
       data[:votes] -= 1 if data[:name] == 'Tuakeu Tangatapoto'
       data
-    }.compact.sort_by { |d| d[:votes] }.reverse.take(1)
+    }.compact.sort_by { |d| d[:votes] }.reverse.first
+    winner.merge!  wikidata( winner[:wikipedia_title] )
     puts winner
     ScraperWiki.save_sqlite([:name, :constituency], winner)
   end
